49e82f9aef97fad3548fd6e6f1d938e6e141ee4c
ds-lukaslangr <ds-lukaslangr>
2022-06-29 13:19:54 +0000

    Fix

---
+++ src/odapcapstone/Task_1/task_1.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+# MAGIC %md-sandbox
+# MAGIC 
+# MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
+# MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
+# MAGIC </div>
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Task 1
+# MAGIC ## Daipe framework basics
+# MAGIC 
+# MAGIC 1. bootstrap
+# MAGIC 2. imports
+# MAGIC 3. decoratorated functions
+# MAGIC 4. 
+
+# COMMAND ----------
+
+# MAGIC %run ../app/bootstrap
+
+# COMMAND ----------
+
+
