c53d45751ca3cc0c6c74cd0029b35b19077cc8d6
fkrjakDatasentics <fkrjakDatasentics>
2022-07-14 09:09:00 +0000

    notebook 1

--- src/odapcapstone/Exercise_01/Exercise 01 - Daipe framework basics.py
+++ src/odapcapstone/Exercise_01/Exercise 01 - Daipe framework basics.py
@@ -173,7 +173,13 @@ display(df_customers)
 
 # COMMAND ----------
 
-# write Daipe code to load customers.csv
+@dp.transformation(
+    dp.read_csv(data_source_path + "/customers.csv",
+                options=dict(header=True)),
+    display=True,
+    )
+def load_customers(df):
+    return df
 
 # COMMAND ----------
 
@@ -213,10 +219,24 @@ display(df_joined)
 # COMMAND ----------
 
 # write Daipe code to load transactions_2022-06-06.csv
+@dp.transformation(
+    dp.read_csv(data_source_path + "/transactions_2022-06-06.csv",
+                options=dict(header=True)),
+    display=True,
+    )
+def load_transactions(df):
+    return df
 
 # COMMAND ----------
 
-# write Daipe code to join customers and transactions_2022-06-06.csv
+@dp.transformation(
+    load_customers,
+    load_transactions,
+    display=True,
+    )
+def join_customers_and_transactions(df, df1):
+    return df.join(df1, "id")
+
 
 # COMMAND ----------
 
@@ -262,7 +282,10 @@ print(db_name)
 
 # COMMAND ----------
 
-# write Daipe code to create a database
+from pyspark.sql import SparkSession
+@dp.notebook_function()
+def create_database(spark: SparkSession):
+    spark.sql(f"create database {'dev_'+db_name}")
 
 # COMMAND ----------
 
@@ -311,7 +334,10 @@ df_joined.write.format("delta").mode("overwrite").option("overwriteSchema", True
 
 # COMMAND ----------
 
-# write Daipe code to save joined customers and transactions to a table
+@dp.transformation(join_customers_and_transactions)
+@dp.table_overwrite(f"{db_name}.customer_transactions")
+def save_customer_transactions(df):
+    return df
 
 # COMMAND ----------
 
