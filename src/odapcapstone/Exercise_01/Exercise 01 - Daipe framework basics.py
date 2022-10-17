a74c1f9be6bdf4d2413eb5f6d4e7c7aed91f53b1
alexandru-narcis.beg@datasentics.com <alexandru-narcis.beg@datasentics.com>
2022-10-17 13:50:02 +0000

    Exercise 1

--- src/odapcapstone/Exercise_01/Exercise 01 - Daipe framework basics.py
+++ src/odapcapstone/Exercise_01/Exercise 01 - Daipe framework basics.py
@@ -174,6 +174,12 @@ display(df_customers)
 # COMMAND ----------
 
 # write Daipe code to load customers.csv
+@dp.transformation(
+    dp.read_csv(data_source_path + "/customers.csv", options=dict(header=True)),
+    display=True
+)
+def load_customers(df):
+    return df
 
 # COMMAND ----------
 
@@ -213,10 +219,23 @@ display(df_joined)
 # COMMAND ----------
 
 # write Daipe code to load transactions_2022-06-06.csv
+@dp.transformation(
+    dp.read_csv(data_source_path + "/transactions_2022-06-06.csv", options=dict(header=True)),
+    display=True
+)
+def load_transactions(df):
+    return df
 
 # COMMAND ----------
 
 # write Daipe code to join customers and transactions_2022-06-06.csv
+@dp.transformation(
+    load_customers,
+    load_transaction,
+    display=True
+)
+def join_customers_and_transactions(first_df, second_df):
+    return first_df.join(second_df, on="id")
 
 # COMMAND ----------
 
@@ -262,7 +281,11 @@ print(db_name)
 
 # COMMAND ----------
 
+from pyspark.sql import SparkSession
 # write Daipe code to create a database
+@dp.notebook_function()
+def create_database(spark: SparkSession):
+    spark.sql(f"CREATE DATABASE IF NOT EXISTS dev_{db_name}")
 
 # COMMAND ----------
 
@@ -312,6 +335,10 @@ df_joined.write.format("delta").mode("overwrite").option("overwriteSchema", True
 # COMMAND ----------
 
 # write Daipe code to save joined customers and transactions to a table
+@dp.transformation(join_customers_and_transactions, display=True)
+@dp.table_overwrite(f"{db_name}.dev_{db_name}customer_transactions")
+def save_customer_transactions(df):
+    return df
 
 # COMMAND ----------
 
